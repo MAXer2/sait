1adc3a0ccb9629d0ff505e3fa8f8e78aceb8486c
Maxer <34571579+MAXer2@users.noreply.github.com>
2020-05-02 18:01:39 +0300

    уникальный дизайн
    н

--- Admin/OneButtonForm.cs
+++ Admin/OneButtonForm.cs
@@ -26,6 +26,7 @@ namespace Сайтец.Admin
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             fontDialog1.Font = btn.Font;
             fontDialog1.ShowDialog();
             btn.Font = fontDialog1.Font;
@@ -36,16 +37,49 @@ namespace Сайтец.Admin
                 "'" + btn.GetType() + "', " +  //Тип
                 "'" + btn.Name + "', " +  //Имя
                 "'" + btn.ParentForm.Name + "', " +  //Имя формы
-                "'FontName', " +
+                "'FontName', '" +
                 btn.Font.Name +
                 "')");
             Form1.Update("INSERT INTO UniqueDesign(ObjectType, ObjectName, Form, Parameter, Value) VALUES(" +
                 "'" + btn.GetType() + "', " +  //Тип
                 "'" + btn.Name + "', " +  //Имя
                 "'" + btn.ParentForm.Name + "', " +  //Имя формы
-                "'FontSize', " +
-                btn.Font.Size.ToString() + 
+                "'FontSize', '" +
+                btn.Font.Size.ToString() +
                 "')");
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+
+                btn.BackgroundImage = Image.FromFile(openFileDialog1.FileName);
+                openFileDialog1.ShowDialog();
+                btn.Font = fontDialog1.Font;
+
+                string fileName =
+                    System.IO.Path.GetFileName(openFileDialog1.FileName);
+                System.IO.File.Copy(openFileDialog1.FileName, fileName);
+
+                Form1.Update("DELETE FROM UniqueDesign"
+                    + " WHERE ObjectName = '" + btn.Name +
+                    "' AND Form = '" + btn.ParentForm.Name +
+                    "' AND Parameter = 'Image'");
+
+                Form1.Update("INSERT INTO UniqueDesign(ObjectType, ObjectName, Form, Parameter, Value) VALUES(" +
+                    "'" + btn.GetType() + "', " +  //Тип
+                    "'" + btn.Name + "', " +  //Имя
+                    "'" + btn.ParentForm.Name + "', " +  //Имя формы
+                    "'Image', '" +
+                    openFileDialog1.FileName +
+                    "')"); ;
+
+            }
         }
     }
 }
